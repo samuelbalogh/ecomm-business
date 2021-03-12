a04a59263c6f9ad583472f8e9b28b5b9f86de9aa
Samu B <samubalogh@gmail.com>
2021-03-12 19:23:22 +0000

    add sql which loads fixtures

---
+++ fixtures.sql
@@ -0,0 +1,7 @@
+.mode csv
+.import data/products.csv products
+.import data/promotions.csv promotions
+.import data/product_promotions.csv product_promotions
+.import data/orders.csv orders
+.import data/commissions.csv commissions
+.import data/order_lines.csv order_lines
