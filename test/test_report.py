d10446057153759536a28a92496616b361688ea1
Samu B <samubalogh@gmail.com>
2021-03-13 17:23:10 +0000

    add basic test cases

--- test/test_report.py
+++ test/test_report.py
@@ -1,8 +1,41 @@
+from unittest.mock import patch
+import sqlite3
+
 import pytest
 
-class TestReport:
-    def test_invalid_date(self):
-        pass
+from app import report, SQL_SELECT_REPORT_FOR_DATE
 
-    def test_valid_date(self):
-        pass
+
+class TestReport:
+    @pytest.mark.parametrize(
+        'date', [
+            '2019-00-00',
+            '0000-00-00',
+            'invalid date here',
+    ])
+    def test_invalid_date(self, date):
+        assert report(date) == ('Invalid date', 400)
+
+    @pytest.mark.parametrize(
+        'date', [
+            '2019-01-01',
+            '1982-11-14',
+    ])
+    @patch('app.get_report_for_date')
+    def test_valid_date(self, mock_report, date):
+        mock_report.return_value = []
+
+        assert report(date) == (
+            '{}', 200, {'content-type': 'application/json'}
+        )
+
+    def test_sql_query_smoke_test(self):
+        '''
+        Run the SQL statement on an empty DB just to ensure
+        there are no syntax errors
+        '''
+        con = sqlite3.connect('test.db')
+        cur = con.cursor()
+        date = '2021-03-12'
+
+        cur.execute(SQL_SELECT_REPORT_FOR_DATE, (date,)).fetchone()
