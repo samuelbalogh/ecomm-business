6ab1cd88dd2f6fe7bd8e02f74d5b6e938cb44b5e
Samu B <samubalogh@gmail.com>
2021-03-13 17:22:54 +0000

    minor refactor

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ from flask import Flask, Response
 
 app = Flask(__name__)
 
-# TODO get commission data
+DB_NAME = 'ecomm.db'
+
+# NOTE: commission per promotion data is missing due to lack of time
 SQL_SELECT_REPORT_FOR_DATE = """
     WITH order_totals AS (
         SELECT order_id, SUM(order_lines.total_amount) AS order_total FROM order_lines GROUP BY order_id
@@ -14,7 +16,7 @@ SQL_SELECT_REPORT_FOR_DATE = """
     commissions_per_order AS (
         SELECT
           commissions.date,
-          SUM(commissions.rate) / COUNT(orders.id)
+          SUM(commissions.rate) / COUNT(orders.id) AS commission_per_order
         FROM commissions, orders
         WHERE commissions.date = date(orders.created_at)
         GROUP BY
@@ -22,11 +24,12 @@ SQL_SELECT_REPORT_FOR_DATE = """
     )
     SELECT
         SUM(order_lines.quantity) AS items,
-        COUNT(orders.customer_id) AS customers,
+        COUNT(DISTINCT orders.customer_id) AS customers,
         SUM(order_lines.discounted_amount) AS total_discount_amount,
         AVG(order_lines.discount_rate) AS discount_rate_avg,
         AVG(order_totals.order_total) AS order_total_avg,
-        SUM(commissions.rate) AS total_commission
+        SUM(commissions.rate) AS total_commission,
+        commissions_per_order.commission_per_order AS order_average
     FROM
         orders, order_lines, order_totals, commissions, commissions_per_order
     WHERE
@@ -38,27 +41,40 @@ SQL_SELECT_REPORT_FOR_DATE = """
 """
 
 
+def get_report_for_date(date, db_name=DB_NAME):
+    try:
+        con = sqlite3.connect(DB_NAME)
+        cur = con.cursor()
+        row = cur.execute(SQL_SELECT_REPORT_FOR_DATE, (date,)).fetchone()
+    except sqlite3.ProgrammingError:
+        # ensure connection is closed even if an error occurs
+        # TODO logging
+        con.close()
+        raise
+
+    field_names = [description[0] for description in cur.description]
+    results = [i for i in zip(field_names, row)]
+
+    return results
+
+
 @app.route('/<date>')
 def report(date):
-    con = sqlite3.connect('ecomm.db')
-    cur = con.cursor()
-
     try:
-        # TODO better way of ensuring date format
+        # ensure the date param is a date string
+        # in production this could be done by defining a schema
+        # eg.: with Marshmallow, or using the OpenAPI spec and Swagger
         datetime.date.fromisoformat(date)
     except ValueError:
         # TODO logging
-        return Response("Invalid date", status=400)
+        return "Invalid date", 400
 
-    row = cur.execute(SQL_SELECT_REPORT_FOR_DATE, (date,)).fetchone()
-    field_names = [description[0] for description in cur.description]
+    try:
+        results = get_report_for_date(date)
+    except Exception:
+        return "Server error", 500
 
-    results = [i for i in zip(field_names, row)]
-
-    return Response(
-        json.dumps(dict(results)),
-        content_type='application/json'  # TODO how to set this automatically
-    )
+    return json.dumps(dict(results)), 200, {'content-type': 'application/json'}
 
 
 if __name__ == '__main__':
--- fixtures.sql
+++ fixtures.sql
@@ -1,7 +1,7 @@
 .mode csv
-.import data/products.csv products
-.import data/promotions.csv promotions
-.import data/product_promotions.csv product_promotions
-.import data/orders.csv orders
-.import data/commissions.csv commissions
-.import data/order_lines.csv order_lines
+.import --skip 1 data/products.csv products
+.import --skip 1 data/promotions.csv promotions
+.import --skip 1 data/product_promotions.csv product_promotions
+.import --skip 1 data/orders.csv orders
+.import --skip 1 data/commissions.csv commissions
+.import --skip 1 data/order_lines.csv order_lines
